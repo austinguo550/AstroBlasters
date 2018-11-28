f9207c5706cb17e0391f6de09ff01f3f74de69da
Sanketh Hegde <san2heg@gmail.com>
2018-11-27 19:38:02 -0800

    Basic collision detection

--- main-scene.js
+++ main-scene.js
@@ -5,7 +5,7 @@ class Project extends Scene_Component
         if( !context.globals.has_controls   ) 
           context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
 
-        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,0,5 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
+        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,0,5 ), Vec.of( 0,2,0 ), Vec.of( 0,1,0 ) );
 
         const r = context.width/context.height;
         context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
@@ -23,14 +23,35 @@ class Project extends Scene_Component
           }
 
         this.lights = [ new Light( Vec.of( -5,5,5,1 ), Color.of( 0,1,1,1 ), 100000 ) ];
+
+        this.earth_radius = 1;
+        this.planet_radius = 0.2;
+        this.bullet_radius = 0.08;
+        this.bullet_transform = Mat4.identity().times(Mat4.scale([this.bullet_radius, this.bullet_radius, this.bullet_radius]));
+        this.planet_transform = Mat4.identity().times(Mat4.translation([0,3,0]))
+                                               .times(Mat4.scale([this.planet_radius, this.planet_radius, this.planet_radius]));
       }
-    make_control_panel()
-      {}
     display( graphics_state )
       { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
 
+        // Draw Earth
         this.shapes.sphere.draw(graphics_state, Mat4.identity(), this.materials.earth);
+
+        // Draw bullets
+        this.bullet_transform = this.bullet_transform.times(Mat4.translation([0,5*dt,0]));
+        this.shapes.sphere.draw(graphics_state, this.bullet_transform, this.materials.phong);
+
+        // Detect collisions
+        let bullet_coords = Vec.of(this.bullet_transform[0][3], this.bullet_transform[1][3], this.bullet_transform[2][3]);
+        let planet_coords = Vec.of(this.planet_transform[0][3], this.planet_transform[1][3], this.planet_transform[2][3]);
+        let dist = Math.sqrt((bullet_coords[0]-planet_coords[0])**2 + (bullet_coords[1]-planet_coords[1])**2 + (bullet_coords[2]-planet_coords[2])**2);
+        let radius_sum = this.planet_radius + this.bullet_radius;
+
+        if (dist >= radius_sum) {
+            // Draw planets
+            this.shapes.sphere.draw(graphics_state, this.planet_transform, this.materials.phong);
+        }
       }
   }
 
